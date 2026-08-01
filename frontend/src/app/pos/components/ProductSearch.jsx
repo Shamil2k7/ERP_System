b80ad7532c8beec86be351470de0fd71fee8c2dd
salmanfariskz <kzzsalman5@gmail.com>
2026-08-01 12:12:36 +0530

    folders created

---
+++ frontend/src/app/pos/components/ProductSearch.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function ProductSearch(){
+
+return(
+
+<div className="mb-4">
+
+<input
+
+type="text"
+
+placeholder="Search product..."
+
+className="border p-3 rounded w-full"
+
+/>
+
+</div>
+
+)
+
+}
