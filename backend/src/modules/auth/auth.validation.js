6970bced8c1118df11d48e12af2a2befa4ad0b63
anasmuhammed-2k25 <muhammedanas8727@gmail.com>
2026-08-03 10:44:33 +0530

    set the backend for signup

--- backend/src/modules/auth/auth.validation.js
+++ backend/src/modules/auth/auth.validation.js
@@ -1,18 +1,62 @@
-const { signupSchema } = require("./auth.schema");
+const {
+    signupSchema,
+    sendOTPSchema,
+    verifyOTPSchema
+} = require("./auth.schema");
 
-//signup middileware
+// ==========================
+// Validate Send OTP
+// ==========================
+const validateSendOTP = (req, res, next) => {
 
-const validateSignup = (req, res, next) => {
-    const { error } = signupSchema.validate(req.body);
+    const { error } = sendOTPSchema.validate(req.body);
+
     if (error) {
         return res.status(400).json({
             success: false,
             message: error.details[0].message
         });
     }
+
+    next();
+};
+
+// ==========================
+// Validate Verify OTP
+// ==========================
+const validateVerifyOTP = (req, res, next) => {
+
+    const { error } = verifyOTPSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: error.details[0].message
+        });
+    }
+
+    next();
+};
+
+// ==========================
+// Validate Signup
+
+const validateSignup = (req, res, next) => {
+
+    const { error } = signupSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: error.details[0].message
+        });
+    }
+
     next();
 };
 
 module.exports = {
+    validateSendOTP,
+    validateVerifyOTP,
     validateSignup
 };
