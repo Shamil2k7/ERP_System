f45aa6056edea8a9591ad33939be49f77581c95f
anasmuhammed-2k25 <muhammedanas8727@gmail.com>
2026-08-04 13:13:47 +0530

    set the Schema erorr

--- backend/src/modules/auth/auth.repository.js
+++ backend/src/modules/auth/auth.repository.js
@@ -1,13 +1,27 @@
 import prisma from "../../config/prisma.js";
 
+
 // ==========================
 // Find User By Email
 // ==========================
 const findUserByEmail = async (email) => {
+
     return await prisma.user.findUnique({
         where: {
-            email: email
+            email
         }
+    });
+
+};
+
+
+
+// ==========================
+// Find Role By Name
+// ==========================
+const findRoleByName = async (name) => {
+    return await prisma.role.findUnique({
+        where: { name }
     });
 };
 
@@ -15,62 +29,135 @@ const findUserByEmail = async (email) => {
 // Create User
 // ==========================
 const createUser = async (userData) => {
+
     return await prisma.user.create({
-        data: userData
-    });
-};
 
-// Save OTP
-
-const saveOTP = async (otpData) => {
-    return await prisma.emailOTP.create({
-        data: otpData
-    });
-};
-
-// Find Latest Unused OTP
-
-const findOTPByEmail = async (email) => {
-    return await prisma.emailOTP.findFirst({
-        where: {
-            email: email,
-            isUsed: false
-        },
-        orderBy: {
-            createdAt: "desc"
-        }
-    });
-};
-
-
-// Mark OTP As Used
-
-const markOTPAsUsed = async (id) => {
-    return await prisma.emailOTP.update({
-        where: {
-            id: id
-        },
         data: {
-            isUsed: true
+
+            email: userData.email,
+
+            fullName: userData.fullName || "Employee",
+
+            employeeId: userData.employeeId,
+
+            phone: userData.phone,
+
+            passwordHash: userData.passwordHash,
+
+            ...(userData.roleId ? {
+                role: {
+                    connect: {
+                        id: userData.roleId
+                    }
+                }
+            } : {}),
+
+            isVerified: true
+
         }
+
     });
+
 };
 
-// Find Verified OTP
 
-const findVerifiedOTP = async (email) => {
+
+// ==========================
+// Save OTP
+// ==========================
+const saveOTP = async (otpData) => {
+
+    return await prisma.emailOTP.create({
+
+        data: otpData
+
+    });
+
+};
+
+
+
+// ==========================
+// Find Latest Unused OTP
+// ==========================
+const findOTPByEmail = async (email) => {
+
     return await prisma.emailOTP.findFirst({
+
         where: {
-            email: email,
-            isUsed: true
+
+            email,
+
+            isUsed:false
+
         },
+
         orderBy: {
-            createdAt: "desc"
+
+            createdAt:"desc"
+
         }
+
     });
+
 };
+
+
+
+// ==========================
+// Mark OTP As Used
+// ==========================
+const markOTPAsUsed = async (id) => {
+
+    return await prisma.emailOTP.update({
+
+        where: {
+
+            id
+
+        },
+
+        data: {
+
+            isUsed:true
+
+        }
+
+    });
+
+};
+
+
+
+// ==========================
+// Find Verified OTP
+// ==========================
+const findVerifiedOTP = async(email)=>{
+
+    return await prisma.emailOTP.findFirst({
+
+        where: {
+
+            email,
+
+            isUsed:true
+
+        },
+
+        orderBy: {
+
+            createdAt:"desc"
+
+        }
+
+    });
+
+};
+
+
 
 export {
+    findRoleByName,
     findUserByEmail,
     createUser,
     saveOTP,
--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import crypto from "crypto";
+
 import { sendOTPEmail } from "../../config/mail.js";
+
 import {
     findUserByEmail,
     createUser,
@@ -8,74 +10,100 @@ import {
     findOTPByEmail,
     markOTPAsUsed,
     findVerifiedOTP,
+    findRoleByName
 } from "./auth.repository.js";
 
 // Send OTP
+
 const sendOTPService = async (email) => {
     const existingUser = await findUserByEmail(email);
     if (existingUser) {
         throw new Error("Email already registered");
     }
-    const otp = crypto.randomInt(100000, 999999).toString();
-    const expiresAt = new Date(Date.now() + 5 * 60 * 1000);
+    const otp = crypto
+        .randomInt(100000, 999999)
+        .toString();
+    const expiresAt = new Date(
+        Date.now() + 5 * 60 * 1000
+    );
     await saveOTP({
         email,
         otp,
-        expiresAt,
+        expiresAt
     });
-    // Send OTP to Email
     await sendOTPEmail(email, otp);
     return {
-        success: true,
-        message: "OTP sent successfully",
+        success:true,
+        message:"OTP sent successfully"
     };
 };
 // Verify OTP
-
 const verifyOTPService = async (email, otp) => {
     const savedOTP = await findOTPByEmail(email);
-    if (!savedOTP) {
+    if(!savedOTP){
         throw new Error("OTP not found");
     }
-    if (savedOTP.isUsed) {
+    if(savedOTP.isUsed){
         throw new Error("OTP already used");
     }
-    if (savedOTP.expiresAt < new Date()) {
+    if(savedOTP.expiresAt < new Date()){
         throw new Error("OTP expired");
     }
-    if (savedOTP.otp !== otp) {
+    if(savedOTP.otp !== otp){
         throw new Error("Invalid OTP");
     }
     await markOTPAsUsed(savedOTP.id);
     return {
-        success: true,
-        message: "OTP verified successfully",
+        success:true,
+        message:"OTP verified successfully"
     };
 };
+
 // Signup
-const signupService = async (userData) => {
-    const existingUser = await findUserByEmail(userData.email);
-    if (existingUser) {
-        throw new Error("Email already registered");
+
+const signupService = async (userData)=>{
+    const existingUser = await findUserByEmail(
+        userData.email
+    );
+    if(existingUser){
+        throw new Error(
+            "Email already registered"
+        );
     }
-    const verifiedOTP = await findVerifiedOTP(userData.email);
-    if (!verifiedOTP) {
-        throw new Error("Please verify your email first");
+    const verifiedOTP = await findVerifiedOTP(
+        userData.email
+    );
+    if(!verifiedOTP){
+        throw new Error(
+            "Please verify your email first"
+        );
     }
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const hashedPassword = await bcrypt.hash(
+        userData.password,
+        10
+    );
+
+    let finalRoleId = userData.roleId;
+
+    if (!finalRoleId) {
+        const empRole = await findRoleByName("Employee");
+        if (empRole) {
+            finalRoleId = empRole.id;
+        }
+    }
+
     const user = await createUser({
-        fullName: userData.fullName || null,
-        email: userData.email,
-        employeeId: userData.employeeId,
-        phone: userData.phone,
-        passwordHash: hashedPassword,
-        roleId: userData.roleId || null,
-        isVerified: true,
+        email:userData.email,
+        employeeId:userData.employeeId,
+        phone:userData.phone,
+        passwordHash:hashedPassword,
+        // USER role UUID
+        roleId:finalRoleId
     });
     return user;
 };
 export {
     sendOTPService,
     verifyOTPService,
-    signupService,
+    signupService
 };
