f7e87b44605e31675a1bb91b30d271b43ba24610
salmanfariskz <kzzsalman5@gmail.com>
2026-08-17 10:18:47 +0530

    added

--- frontend/src/app/sales/components/SalesTable.jsx
+++ frontend/src/app/sales/components/SalesTable.jsx
@@ -122,12 +122,7 @@ export default function SalesTable({ sales = [] }) {
                       View
                     </Link>
 
-                    <Link
-                      href={`/sales/edit/${sale.id}`}
-                      className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600"
-                    >
-                      Edit
-                    </Link>
+                    
 
                   </div>
 
